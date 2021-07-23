b61cd3fe984392b5e3d10ddfa5be10eeb95d7c22
ashikvarma11 <varmaashik@gmail.com>
2021-07-23 14:58:27 +0530

    removed console.log

--- news-extension/src/components/Home/Home.tsx
+++ news-extension/src/components/Home/Home.tsx
@@ -48,7 +48,6 @@ const Home: React.FC<Props> = ({ interests }) => {
           setIsLoading(false)
         })
         .catch((error) => {
-          console.log(error,'hello')
           setIsLoading(false)
         })
     } else {
